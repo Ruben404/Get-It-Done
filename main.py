52176d1b49c4309477e44328be8b6bac919b9584
Ruben Lazarin <rubenlazarin03@gmail.com>
2019-05-13 16:46:09 -0500

    added sqlalchemy and pymysql. started class Task(db.Model)

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import AQLAlchemy
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:get-it-done@localhost:8889/get-it-done'
+#.....................................................://[_username_]:[password]@[__server__]/[database name]
+
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
 
 tasks = []
 
